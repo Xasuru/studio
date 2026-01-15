3fe892d94d61b7d1e51d2d9b1b30db3845df6980
Xasuru <aeolusroblox@gmail.com>
2026-01-15 20:59:16 +0800

    Fixed app code

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1910884817-a3276",
-  "appId": "1:520320183393:web:81e639046870a8c4308e20",
-  "apiKey": "AIzaSyDbG7pvnZrmu1H6cVPdCtJF3OJEquOoonQ",
-  "authDomain": "studio-1910884817-a3276.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "520320183393"
-};
